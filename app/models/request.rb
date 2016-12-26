85558303889f130fbf1f8070ac245fd17dccc461
Aishwarya <aishwarya.s@roadrunnr.in>
2016-12-27 00:33:30 +0530

    Using textlocal for sending sms.

--- app/models/request.rb
+++ app/models/request.rb
@@ -9,13 +9,14 @@ class Request < ActiveRecord::Base
 
   def broadcast_requests
     user_details = UserDetail.fetch_users_in_locality(self.locality)
-    sms_requests(user_details.map(&:phone_no).compact)
+    sms_requests(user_details.map(&:phone_no).compact.join(","))
     email_requests(user_details.map(&:user))
   end
 
   def sms_requests(phone_numbers)
-    sms_string = "Hi! #{self.user.user_detail.try(:name)} is in need of a scribe on #{self.start_time.to_date} at #{self.address}. We have emailed you the details!"
-    phone_numbers.each{|phone_number| Sms.send_exotel_sms(phone_number, sms_string)}
+    sms_string = "Hi! #{self.user.user_detail.try(:name)} is in need of a scribe on #{self.start_time.to_date} at #{self.address}. Please click on LINK to confirm."
+    Sms.new(sms_string, phone_numbers).send
+      #phone_numbers.each{|phone_number| Sms.send_exotel_sms(phone_number, sms_string)}
   end
 
   def email_requests(users)
--- app/models/sms.rb
+++ app/models/sms.rb
@@ -1,12 +1,34 @@
 class Sms
-  EXOTEL_NUMBER = '09243422233'
-  def self.send_exotel_sms(phone_number, sms_string)
-    begin
-      response = Exotel::Sms.send(:from => EXOTEL_NUMBER, :to => phone_number,
-                                  :body => sms_string, :priority => 1)
-    rescue => error
-      Rails.logger.error "Failed to send sms to phone_number:" + phone_number.to_s
-    end
-    Rails.logger.debug "Sending sms! #{response.inspect}"
+  # EXOTEL_NUMBER = '09243422233'
+  # def self.send_exotel_sms(phone_number, sms_string)
+  #   begin
+  #     response = Exotel::Sms.send(:from => EXOTEL_NUMBER, :to => phone_number,
+  #                                 :body => sms_string, :priority => 1)
+  #   rescue => error
+  #     Rails.logger.error "Failed to send sms to phone_number:" + phone_number.to_s
+  #   end
+  #   Rails.logger.debug "Sending sms! #{response.inspect}"
+  # end
+
+  def initialize(message, recipients)
+    @message = message
+    @recipients = recipients
+    @config = load_config
+  end
+
+  def send
+    uri = URI.parse(request_url)
+    http = Net::HTTP.start(uri.host, uri.port)
+    request = Net::HTTP::Get.new(uri.request_uri)
+    res = Net::HTTP.post_form(uri, 'username' => @config[:username], 'hash' => @config[:hash], 'message' => @message, 'sender' => 'Team Scribe', 'numbers' => @recipients)
+  end
+
+  private
+  def load_config
+    YAML.load_file(File.join(Rails.root, "config/textlocal.yml")).deep_symbolize_keys!
+  end
+
+  def request_url
+    'http://api.textlocal.in/send/?'
   end
 end
